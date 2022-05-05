87d186dd7ed26f971a8d8d53bedfd40f78b0eb46
Riki Takizawa <rikitakizawa@kentonoMacBook-Pro-11511.local>
2022-05-05 12:49:41 +0900

    add Options with clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(
+    name = "td",
+    author = "Takizawa Riki",
+    version = "0.1.0",
+    about = "Command Line Tool to manage your To Do."
+)]
+
+struct Options {
+    #[clap(short = 'a', long = "add", value_name = "NEW_TO_DO", help = "add NEW_TO_DO to your To Do list.")]
+
+    #[clap(short = 'f', long = "finish", value_name = "FINISHED_TO_DO", help = "put a check mark to FINISHED_TO_DO of your To Do list.")]
+
+    #[clap(short = 'd', long = "delete", value_name = "DELETED_TO_DO", help = "delete DELETED_TO_DO from your To Do list.")]
+
+    #[clap(short = 'e', long = "edit", value_name = "EDITED_TO_DO", help = "change EDITED_TO_DO of your To Do list to NEW_TO_DO.")]
+}
+
 fn main() {
+
     println!("Hello, world!");
 }
