f30aafd22f096747165fd6f221a4cb4106568c2a
Riki Takizawa <rikitakizawa@kentonoMacBook-Pro-11511.local>
2022-05-06 11:44:13 +0900

    I added build.yaml and changed main.rs

--- src/main.rs
+++ src/main.rs
@@ -30,3 +30,17 @@ fn main() {
     let options = Options::parse();
 
 }
+
+
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
+    }
+}
